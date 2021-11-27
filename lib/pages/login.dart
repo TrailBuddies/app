3352c97fe6bf84d7a8cc7e525b84b5d09488e7f3
matievisthekat <matievisthekat@gmail.com>
2021-11-27 22:12:55 +0200

    add login page with not functionality

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:trail_buddies/widgets/landing_page/lets_go_button.dart';
+import 'package:trail_buddies/widgets/common/button.dart';
+import 'package:trail_buddies/widgets/common/text_input.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: const EdgeInsets.all(32),
         decoration: const BoxDecoration(
-          color: Color.fromRGBO(0, 0, 0, 1),
+          color: Color.fromRGBO(232, 73, 23, 1),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const TextInput(hintText: "Email"),
+              const SizedBox(height: 16),
+              const TextInput(hintText: "Password"),
+              const SizedBox(height: 30),
+              Button(
+                text: 'Login',
+                backgroundColour: Colors.green.shade400,
+                textColour: Colors.white,
+                onTap: () => {},
+              )
+            ],
+          ),
         ),
       ),
     );
