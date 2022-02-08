ea933c8cfd417059451731085b334f7fdae5ff85
matievisthekat <matievisthekat@gmail.com>
2022-02-08 21:10:47 +0200

    add SilverAppBar to home screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 150.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Container(
+                width: double.infinity,
+                height: 50,
+                margin: const EdgeInsets.only(left: 10, right: 10),
+                child: const Text('Trail Buddies'),
+              ),
+              centerTitle: true,
+              background: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/background_image.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverList(delegate: SliverChildListDelegate([]))
+        ],
+      ),
+    );
   }
 }
