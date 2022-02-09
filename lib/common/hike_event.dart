5e77150600e5552c39de00de8c33953cb3cb3a97
matievisthekat <matievisthekat@gmail.com>
2022-02-09 20:52:10 +0200

    add hike_event class
    no methods or special things right now

---
+++ lib/common/hike_event.dart
@@ -0,0 +1,33 @@
+abstract class HikeEvent {
+  late String id;
+  late String title;
+  late String description;
+  late List<DateTime> duration;
+  late double lat;
+  late double lng;
+  late int difficulty;
+  late DateTime createdAt;
+  late DateTime updatedAt;
+  late String userId;
+
+  HikeEvent({
+    required this.id,
+    required this.title,
+    required this.description,
+    required String duration,
+    required String lat,
+    required String lng,
+    required String difficulty,
+    required String createdAt,
+    required String updatedAt,
+    required this.userId,
+  }) {
+    this.duration =
+        duration.split('...').map((d) => DateTime.parse(d)).toList();
+    this.lat = double.parse(lat);
+    this.lng = double.parse(lng);
+    this.difficulty = int.parse(difficulty);
+    this.createdAt = DateTime.parse(createdAt);
+    this.updatedAt = DateTime.parse(updatedAt);
+  }
+}
