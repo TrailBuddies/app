d1d549544257ae1828001a1ea24034fab74cb17a
matievisthekat <matievisthekat@gmail.com>
2022-02-10 16:40:10 +0200

    add HikeEvent.all() static method to fetch all hikes

--- lib/hike_event.dart
+++ lib/hike_event.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
 import 'package:trail_buddies/user.dart';
+import './declarations.dart';
 
 class HikeEvent {
   late String id;
@@ -35,5 +39,36 @@ class HikeEvent {
 
   Future<User?> getUser() async {
     return await User.fetch(userId);
+  }
+
+  static Future<List<HikeEvent>> all() async {
+    final response = await get(
+      Uri.parse('$baseUrl/api/v1/hike_events'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    final json = jsonDecode(response.body);
+
+    if (response.statusCode != 200) {
+      throw Exception(json['error'] ??
+          'Failed to fetch /api/v1/hike_events. Failed with status ${response.statusCode} and response: ${json.toString()}');
+    } else {
+      return (json as List<Map<String, String>>)
+          .map((e) => HikeEvent(
+                id: e['id'] as String,
+                title: e['title'] as String,
+                description: e['description'] as String,
+                duration: e['duration'] as String,
+                lat: e['lat'] as String,
+                lng: e['lng'] as String,
+                difficulty: e['difficulty'] as String,
+                createdAt: e['created_at'] as String,
+                updatedAt: e['updated_at'] as String,
+                userId: e['user_id'] as String,
+              ))
+          .toList();
+    }
   }
 }
