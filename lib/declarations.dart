afd5ea29beb82828e34e640123b5401461debee8
matievisthekat <matievisthekat@gmail.com>
2022-03-09 12:00:08 +0200

    add route matching for '/hike/:id'

--- lib/declarations.dart
+++ lib/declarations.dart
@@ -1,1 +1,3 @@
 const baseUrl = 'http://10.0.2.2:3000';
+const uuidRegex =
+    '[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}';
--- lib/routing.dart
+++ lib/routing.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './declarations.dart';
 import './pages/check_login/widget.dart';
 import './pages/login.dart';
 import './pages/me.dart';
@@ -16,6 +17,13 @@ Widget getPageWidget(String name) {
 
   if (name == '/me') {
     return const MePage();
+  }
+
+  if (RegExp('^/hike/$uuidRegex').hasMatch(name)) {
+    final uri = Uri.parse(name);
+    final uuid = uri.pathSegments.last;
+
+    // TODO: create hike page and return it here
   }
 
   return const CheckLogin();
