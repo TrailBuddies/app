40d7df929c206cab9a5abc216114f7791945b5d4
matievisthekat <matievisthekat@gmail.com>
2022-06-13 09:44:39 +0200

    show all hike info in a list for now
    this is a very basic display page but itll do for now

--- lib/pages/hike.dart
+++ lib/pages/hike.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 
 import 'package:trail_buddies/hike_event.dart';
@@ -44,14 +45,51 @@ class _HikePageState extends State<HikePage> {
               ? const Center(
                   child: Text('No hike found'),
                 )
-              : Center(
-                  child: Column(
-                    children: [
-                      Text(hike!.title),
-                      Text(hike!.description),
-                      Text('${hike!.lat}, ${hike!.lng}'),
-                    ],
-                  ),
+              : Column(
+                  children: [
+                    Image.network(
+                      hike!.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          ListTile(
+                            title: Text(hike!.title),
+                            subtitle: Text(hike!.description),
+                          ),
+                          ListTile(
+                            title: const Text('Duration'),
+                            subtitle: Text(
+                              hike!.duration
+                                  .map((d) => DateFormat.yMd().format(d))
+                                  .join('...'),
+                            ),
+                          ),
+                          ListTile(
+                            title: const Text('Latitude'),
+                            subtitle: Text(hike!.lat.toString()),
+                          ),
+                          ListTile(
+                            title: const Text('Longitude'),
+                            subtitle: Text(hike!.lng.toString()),
+                          ),
+                          ListTile(
+                            title: const Text('Difficulty'),
+                            subtitle: Text(hike!.difficulty.toString()),
+                          ),
+                          ListTile(
+                            title: const Text('Created at'),
+                            subtitle: Text(hike!.createdAt.toString()),
+                          ),
+                          ListTile(
+                            title: const Text('Updated at'),
+                            subtitle: Text(hike!.updatedAt.toString()),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
     );
   }
