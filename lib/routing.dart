f2863aaa7cff43e6c39717156c33db45b139e656
matievisthekat <matievisthekat@gmail.com>
2022-10-07 11:58:53 +0200

    reorganise import order

--- lib/routing.dart
+++ lib/routing.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:trail_buddies/pages/hike.dart';
 
-import './declarations.dart';
 import './pages/check_login/widget.dart';
 import './pages/login.dart';
 import './pages/me.dart';
 import './pages/register.dart';
+import './declarations.dart';
 
 Widget getPageWidget(String name) {
   if (name == '/login') {
